d2803e67bfa106cd7c80a307927af9b30ff53998
nahrstaedt <nahrstaedt@080ed786-a3fb-4c06-a3f7-2dff24be27a0>
2008-06-09 08:26:02 +0000

    about dialog updated

--- trunk/src/main_window.cpp
+++ trunk/src/main_window.cpp
@@ -885,6 +885,7 @@ void QRL_MainWindow::about()
     QMessageBox::about(this,"About QRtaiLab",
 		"Version 0.1\n"
                 "This app was coded for educational purposes.\n\n"
-                "Copyright (C) 2008 Holger Nahrstaedt. \n\n"
+                "Copyright (C) 2008 Holger Nahrstaedt. \n"
+		"              Control system group, TU-Berlin\n"
                 "Have Fun!\n");
 }
